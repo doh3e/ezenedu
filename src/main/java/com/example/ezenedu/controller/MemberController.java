29d9088f4d6518c6ea4b07c7a98f65300ecdd841
doh3e <wldms3333@gmail.com>
2024-06-12 15:33:49 +0900

    지은 - 회원가입 이식 진행중 (24.06.12 15:33)

--- src/main/java/com/example/ezenedu/controller/MemberController.java
+++ src/main/java/com/example/ezenedu/controller/MemberController.java
@@ -1,7 +1,14 @@
 package com.example.ezenedu.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MemberController {
+
+    @GetMapping("/member_join")
+    public String join1(){
+
+        return "member/member_join";
+    }
 }
